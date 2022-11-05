19e371b3b0fdf1af8145eb4526ef10c2aee48561
Athanasios Novatsidis <novatsidi@csd.auth.gr>
2022-11-05 16:13:57 +0200

    Test2

--- src/start/room.java
+++ src/start/room.java
@@ -1,18 +1,15 @@
 package start;
 
-public class A1 {
+public class room {
     private String name;
     private String hoteltype;
     private String area;
     private String description;
 
 
-    /**
-     *
-     * @param args
-     */
+
     public static void main(String[] args) {
-        System.out.println("Begin");
+
 
     }
 
