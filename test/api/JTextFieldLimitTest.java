6ccdb647526a9266cb0ecf958790fd74146a84cc
Athanasios Novatsidis <novatsidi@csd.auth.gr>
2022-12-16 15:07:06 +0200

    Test beginning

--- test/api/JTextFieldLimitTest.java
+++ test/api/JTextFieldLimitTest.java
@@ -2,11 +2,13 @@ package api;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import javax.swing.text.BadLocationException;
 
 public class JTextFieldLimitTest {
 
+
     @Test
-    public void insertString() {
+    public void insertString() throws BadLocationException {
+
     }
 }
--- test/api/ReviewFileTest.java
+++ test/api/ReviewFileTest.java
@@ -2,21 +2,38 @@ package api;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ReviewFileTest {
-
+ ReviewFile check= new ReviewFile();
     @Test
-    public void addR() {
+    public void addR() throws IOException {
+       byte[] array = new byte[7]; // length is bounded by 7
+       new Random().nextBytes(array);
+       String generatedString = new String(array, Charset.forName("UTF-8"));
+        check.AddR(generatedString,"Nikolaos","Heritage","Teleio",5);
+    }
+   @Test
+   public void searchReview() throws FileNotFoundException {
+      ArrayList wow = new ArrayList<>();
+      wow.add("Heritage Hotel   Isaia3 Greece 52100 5");
+      assertEquals(check.SearchReviewofUser("Wow"),wow);
+
+   }
+    @Test
+    public void getmo() throws IOException {
+
+       assertTrue(check.getmo()==0);
     }
 
-    @Test
-    public void getmo() {
-    }
 
-    @Test
-    public void searchReview() {
-    }
 
     @Test
     public void searchReviewforUser() {
